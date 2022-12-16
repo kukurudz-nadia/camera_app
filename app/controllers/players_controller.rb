ffb6ee1528bc1d786f51ca65a17dca55aa1393fc
Nadia <kukurudz.nadia6@gmail.com>
2022-12-16 22:57:30 +0200

    created show html, updated method show in players_controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#
+
 class PlayersController < ApplicationController
   before_action :set_player, only: %i[show edit update destroy]
   def index
@@ -12,7 +12,21 @@ class PlayersController < ApplicationController
 
   def edit; end
 
-  def create; end
+  def create
+    @player = Player.create(players_params)
+
+    PictureAttachmentService.attach(@player, params['player']['player_picture'])
+
+    respond_to do |format|
+      if @player.save
+        format.html { redirect_to @player, notice: 'Player was successfully created.' }
+        format.json {render :show, status: :created, location: @player }
+      else
+        format.html { render :new }
+        format.json { render json: @player.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def update; end
 
@@ -21,5 +35,19 @@ class PlayersController < ApplicationController
   private
   def set_player
     @player = Player.find(params[:id])
+  end
+
+  def players_params
+    params.require(:player).permit(
+      :name,
+      :dob,
+      :pob,
+      :age,
+      :height,
+      :position,
+      :foot,
+      :current_club,
+      :player_picture
+    )
   end
 end
--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -28,5 +28,33 @@ RSpec.describe PlayersController, type: :controller do
       is_expected.to be_successful
       is_expected.to render_template :show
     end
+
+    describe 'POST #create' do
+      let!(:player) { create :player }
+
+      let(:params) do
+        {
+        name: player.name,
+        dob: player.dob,
+        pob: player.pob,
+        age: player.age,
+        height: player.height,
+        position: player.position,
+        foot: player.foot,
+        current_club: player.current_club,
+        picture: Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/1.png'))
+        }
+      end
+
+      subject do
+        post :create, params: params
+        response
+      end
+      context 'with success' do
+        it 'saves player' do
+          is_expected.to be_successful
+        end
+      end
+    end
   end
 end
